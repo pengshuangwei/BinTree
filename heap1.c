903629a224888c3aa20b47f3cdc6d8e30203d87c
shuangwei peng <p2257268451@outlook.com>
2020-08-14 11:05:00 +0800

    完善了建立最大堆的函数

--- heap1.c
+++ heap1.c
@@ -9,15 +9,21 @@ struct HeapStruct {
 	int size;//堆的当前元素个数
 	int capacity;//堆得最大容量
 };
+typedef Heap Maxheap;
+typedef Heap Minheap;
 Maxheap Create(int Maxsize)//创建一个空的最大堆
 {
 	MaxHeap H = malloc(sizeof(struct HeapStruct));
+	H->Elements=(ElementType*)malloc((MaxSize+1)*(sizeof(struct HeapStruct)))
 	H->size = 0;
 	H->capacity = Maxsize;
 	H->Elements[0] = MaxData;//定义哨兵为大于堆中所有可能元素的值，便于以后操作
 	return H;
 }
 Boolean IsFull(Maxheap H);//判断最大堆是否已满
+{
+	return(H->Size == H->Capacity);
+}
 Insert(MaxHeap H, ElementType item);//将元素item插入到最大堆H中
 {
 	int i;
@@ -31,6 +37,9 @@ Insert(MaxHeap H, ElementType item);//将元素item插入到最大堆H中
 	H->Elements[i] = item;//插入
 }
 Booklean IsEmpty(Maxheap，H); 判断最大堆H是否为空
+{
+	return (H->size == 0);
+}
 ElementType DeleteMax(MaxHeap H);//返回H中最大元素
 {
 	int parent, child;
@@ -52,3 +61,32 @@ ElementType DeleteMax(MaxHeap H);//返回H中最大元素
 	H->Elements[parent] = temp;
 	return MAxItem;
 } 
+//--------------建造最大堆_-------------
+void PerDown(MaxHeap H, int P) {
+	//下滤：将H中以H->Data[p]为根的子堆调整为最大堆
+	int parent, child;
+	ElementType x;
+	x = H->Elements[p];/* 取出根结点存放的值 */
+	for (parent = 0; parent * 2 <= H->size; parent = child) {
+		child = parent * 2;
+		if (child != H->size && H->Elements[child] < H->Elements[child + 1])
+			child += 1; /* Child指向左右子结点的较大者 */
+		if (x >= H->Elements[parent]) {
+			break;
+		}
+		else {
+			H->Elements[parent] = H->Elements[child];
+		}
+		H->Elements[parent] = x;
+	}
+}
+void BuildHeap(MaxHeap H)
+{ /* 调整H->Data[]中的元素，使满足最大堆的有序性  */
+  /* 这里假设所有H->Size个元素已经存在H->Data[]中 */
+
+	int i;
+
+	/* 从最后一个结点的父节点开始，到根结点1 */
+	for (i = H->Size / 2; i > 0; i--)
+		PercDown(H, i);
+}
